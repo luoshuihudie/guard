499bf9f4c1129e2984a61a80b918f6d19cbbb678
陈文 <master@chenwen.name>
2017-03-05 15:33:09 +0800

    完善配置文件

--- src/Wayne/Guard/config/permissions.home.php
+++ src/Wayne/Guard/config/permissions.home.php
@@ -4,9 +4,9 @@ return [
     'name'       => '首页',
     'index'      => 'welcome', // 导航页链接地址，routes 节点的键名
     'prefix'     => '', // 路由前缀
-    'namespace' => 'App\Http\Controllers',
+    'namespace'  => 'App\Http\Controllers',
     'middleware' => [
-        'web', 'guard.auth', 
+        'web', 'guard.auth',
         'guard.throttle', 'guard.log',
     ],
     'groups'     => [
@@ -20,16 +20,19 @@ return [
             'name'     => 'Welcome',
             'uri'      => '/',
             'method'   => 'get',
+            'type'     => 'page',
             'uses'     => 'HomeController@welcome',
-            'limit-on' => true, // 权限开关，值为false 则登陆后不限制该功能,默认为 true
-            'throttle' => 100, // 限制单用户最大访问次数，
+            'limit-on' => false,
             'log.file' => '【{{user.name}}】访问了操作 Welcome',
         ],
         'home'    => [
             'name'     => 'Home',
             'uri'      => '/home',
             'method'   => 'get',
+            'type'     => 'menu',
             'uses'     => 'HomeController@home',
+            'limit-on' => true, // 权限开关，值为false 则登陆后不限制该功能,默认为 true
+            'throttle' => 100, // 限制单用户最大访问次数，
             'log.file' => '【{{user.name}}】访问了操作日志页',
         ],
     ],
